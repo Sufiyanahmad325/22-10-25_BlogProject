1ecbf55dcbfaea0427a9e69d81e7b0bd1267d1cd
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-10-09 16:03:28 +0530

    author name has been added

--- src/models/postSchema.models.js
+++ src/models/postSchema.models.js
@@ -12,21 +12,45 @@ const postSchema = new mongoose.Schema({
     required: true
   },
   tags: [String], // optional tags array
+
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true // हर post का author ज़रूरी
   },
-  isPublished: {
-    type: Boolean,
-    default: true // false भी कर सकते हो drafts के लिए
-  }
+
+  authorName: {
+    type: String,
+    required: true
+  },
+
+  // isPublished: {
+  //   type: Boolean,
+  //   default: true // false भी कर सकते हो drafts के लिए
+  // },
+
+
+  blogImage: {
+    type: String,
+  },
+
+  category:{
+    type:String,
+    required:true
+  },
+
+
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"   // इस post को किन-किन users ने like किया
+    }
+  ],
 }, {
   timestamps: true // createdAt, updatedAt
 });
 
-// index for faster queries (optional)
-postSchema.index({ title: "text", content: "text", tags: "text" });
+
 
 const Post = mongoose.model("Post", postSchema);
 export default Post;
