f116ad70a272b42292c5c8a3b23809503eff44f4
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-10-09 16:04:39 +0530

    fix bugs and add features for connet with multer

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import { upload } from '../middleware/multer.middleware.js';
-import { deleteBlog, editBlog, getAllUsers, getCurrentUser, LikeBlog, login, logout, register, uploadBlog } from '../controller/user.controller.js';
+import { deleteBlog, editBlog, getAllUsers, getCurrentUser, LikeBlog, login, logout, register, updateUserProfile, uploadBlog } from '../controller/user.controller.js';
 import { verifyJWT } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -13,13 +13,20 @@ router.route('/register').post(upload.fields([
 
 
 router.route('/login').post(login)
-router.route('/uploadBlog').post(verifyJWT,uploadBlog)
+router.route('/uploadBlog').post(
+    verifyJWT,
+    upload.fields([{ name: 'blogImage', maxCount: 1 }]),
+    uploadBlog
+  )
 router.route('/current-user').get(verifyJWT,getCurrentUser)
 router.route('/edit-blog').post(verifyJWT,editBlog)
 router.route('/delete-blog').post(verifyJWT,deleteBlog)
 router.route('/logout').get(verifyJWT,logout)
 router.route('/like-blog').post(verifyJWT,LikeBlog)
 router.route('/get-all-users').get(verifyJWT,getAllUsers)
+
+
+router.route('/update-user-profile').post(upload.fields([{ name: 'avatar', maxCount: 1 }]) ,verifyJWT,updateUserProfile)
 
 
 
