29e2172a46fc9f762429d083947d635bbe5605d5
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-10-10 12:26:57 +0530

    remove unneccessary code

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,13 +1,15 @@
-import multer from 'multer'
-
+import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '../public/temp')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname)
-    }
-  })
-  
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, path.join(process.cwd(), "public/temp")); // safe absolute path
+  },
+  filename: function (req, file, cb) {
+    const uniqueName =
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  },
+});
+
+export const upload = multer({ storage: storage });
