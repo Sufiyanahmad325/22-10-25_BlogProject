d8ce4c6d0f831a299dc168ec11b9690703205698
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-10-11 06:58:10 +0530

    fix some bugs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import { upload } from '../middleware/multer.middleware.js';
-import { deleteBlog, editBlog, getAllUsers, getCurrentUser, LikeBlog, login, logout, register, updateUserProfile, uploadBlog } from '../controller/user.controller.js';
+import { deleteBlog, editBlog, getAllBlogsWithCurentUser, getAllUsers, LikeBlog, login, logout, register, updateUserProfile, uploadBlog } from '../controller/user.controller.js';
 import { verifyJWT } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -18,7 +18,7 @@ router.route('/uploadBlog').post(
     upload.fields([{ name: 'blogImage', maxCount: 1 }]),
     uploadBlog
   )
-router.route('/current-user').get(verifyJWT,getCurrentUser)
+router.route('/current-user-allUserBlog').get(verifyJWT,getAllBlogsWithCurentUser)
 router.route('/edit-blog').post(verifyJWT,editBlog)
 router.route('/delete-blog').post(verifyJWT,deleteBlog)
 router.route('/logout').get(verifyJWT,logout)
