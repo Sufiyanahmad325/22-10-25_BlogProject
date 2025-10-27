f44ed91e2e15be895baefb69db824a4356d10349
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-10-27 10:29:04 +0530

    fix some error

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,25 +8,25 @@ const router = express.Router();
 
 
 router.route('/register').post(upload.fields([
-    { name: 'avatar', maxCount: 1 }
+  { name: 'avatar', maxCount: 1 }
 ]), register);
 
 
 router.route('/login').post(login)
 router.route('/uploadBlog').post(
-    verifyJWT,
-    upload.fields([{ name: 'blogImage', maxCount: 1 }]),
-    uploadBlog
-  )
-router.route('/current-user-allUserBlog').get(verifyJWT,getAllBlogsWithCurentUser)
-router.route('/edit-blog').post(verifyJWT,editBlog)
-router.route('/delete-blog').post(verifyJWT,deleteBlog)
-router.route('/logout').get(verifyJWT,logout)
-router.route('/like-blog').post(verifyJWT,LikeBlog)
-router.route('/get-all-users').get(verifyJWT,getAllUsers)
+  verifyJWT,
+  upload.fields([{ name: 'blogImage', maxCount: 1 }]),
+  uploadBlog
+)
+router.route('/current-user-allUserBlog').get(verifyJWT, getAllBlogsWithCurentUser)
+router.route('/edit-blog').post(upload.fields([{ name: 'blogImage', maxCount: 1 }]), verifyJWT, editBlog)
+router.route('/delete-blog').post(verifyJWT, deleteBlog)
+router.route('/logout').get(verifyJWT, logout)
+router.route('/like-blog').post(verifyJWT, LikeBlog)
+router.route('/get-all-users').get(verifyJWT, getAllUsers)
 
 
-router.route('/update-user-profile').post(upload.fields([{ name: 'avatar', maxCount: 1 }]) ,verifyJWT,updateUserProfile)
+router.route('/update-user-profile').post(upload.fields([{ name: 'avatar', maxCount: 1 }]), verifyJWT, updateUserProfile)
 
 
 
